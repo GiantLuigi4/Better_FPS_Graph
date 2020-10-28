625571fcf8a04e1f8c7d4619428cee6f765cfa47
GiantLuigi4 <49770992+giantluigi4@users.noreply.github.com>
2020-10-27 21:31:38 -0400

    formatting

--- src/main/java/com/tfc/better_fps_graph/Better_fps_graph.java
+++ src/main/java/com/tfc/better_fps_graph/Better_fps_graph.java
@@ -53,7 +53,7 @@ public class Better_fps_graph {
 					super.setPressed(valueIn);
 					
 					if (valueIn == true) {
-						Profiler.addSection("better_fps_graph:Open Graph", 1,1,0);
+						Profiler.addSection("better_fps_graph:Open Graph", 1, 1, 0);
 						
 						try {
 							Display.disp(frames.get(frames.size() - 1).sections.values());
@@ -78,55 +78,55 @@ public class Better_fps_graph {
 	private void onDrawFPSGraph(RenderGameOverlayEvent event) {
 		if (event instanceof RenderGameOverlayEvent.Pre) {
 			if (event.getType() == RenderGameOverlayEvent.ElementType.FPS_GRAPH) {
-				Profiler.addSection("better_fps_graph:Render Graph", 0,1,0);
-				framesWithout=0;
+				Profiler.addSection("better_fps_graph:Render Graph", 0, 1, 0);
+				framesWithout = 0;
 				lastRender = Util.milliTime();
 				double amp = 1;
 				double scale = 1;
 				
-				if (Minecraft.getInstance().gameSettings.guiScale!=0) {
-					amp = 1f/(Minecraft.getInstance().gameSettings.guiScale);
-					scale=Minecraft.getInstance().gameSettings.guiScale;
+				if (Minecraft.getInstance().gameSettings.guiScale != 0) {
+					amp = 1f / (Minecraft.getInstance().gameSettings.guiScale);
+					scale = Minecraft.getInstance().gameSettings.guiScale;
 				}
 				
-				int cap=(int)(Minecraft.getInstance().getMainWindow().getWidth()*(amp/7));
+				int cap = (int) (Minecraft.getInstance().getMainWindow().getWidth() * (amp / 7));
 				
-				while (frames.size()>=cap) {
+				while (frames.size() >= cap) {
 					frames.remove(0);
 				}
 				
-				int x=1;
+				int x = 1;
 				frames.add(Profiler.getFrame());
-				long y1 = (int)(Minecraft.getInstance().getMainWindow().getHeight()/scale);
+				long y1 = (int) (Minecraft.getInstance().getMainWindow().getHeight() / scale);
 				long min = 9999999999L;
 				long max = 0;
 				
 				for (Frame frame : frames) {
-					long time=0;
+					long time = 0;
 					
 					for (Section section : frame.sections.values()) {
-						time+=section.time;
-						drawRect(x,y1-(time*2),2,section.time*2,section.color[0],section.color[1],section.color[2],1);
+						time += section.time;
+						drawRect(x, y1 - (time * 2), 2, section.time * 2, section.color[0], section.color[1], section.color[2], 1);
 					}
 					
-					min = Long.min(min,time);
-					max = Long.max(max,time);
-					x+=2;
+					min = Long.min(min, time);
+					max = Long.max(max, time);
+					x += 2;
 				}
 				
-				long max_render = max*2;
-				int left = x-(cap*2);
+				long max_render = max * 2;
+				int left = x - (cap * 2);
 				//Boundries of fps graph
-				drawRect(left,y1,cap*2+1,1,1,1,1,1);
-				drawRect(left-1,y1-max_render,1,max_render,1,1,1,1);
-				drawRect(x,y1-max_render,1,max_render,1,1,1,1);
+				drawRect(left, y1, cap * 2 + 1, 1, 1, 1, 1, 1);
+				drawRect(left - 1, y1 - max_render, 1, max_render, 1, 1, 1, 1);
+				drawRect(x, y1 - max_render, 1, max_render, 1, 1, 1, 1);
 				int max1 = Math.max(cap * 2 + 4, x + 4);
-				drawRect(left,y1-((1000f/Minecraft.getInstance().gameSettings.framerateLimit)*2),cap*2,1,0,0,1,1);
-				drawRect(left,y1-(min*2),cap*2,1,0,1,0,1);
-				drawRect(left,y1-(max_render),cap*2,1,1,0,0,1);
-				Minecraft.getInstance().fontRenderer.drawString(Minecraft.getInstance().gameSettings.framerateLimit+" FPS", max1,y1-((1000f/Minecraft.getInstance().gameSettings.framerateLimit)*2)-3,255);
-				Minecraft.getInstance().fontRenderer.drawString("Minimum " + min + " ms", max1,y1-(min*2)-3,65280);
-				Minecraft.getInstance().fontRenderer.drawString("Maximum " + max + " ms", max1,y1-(max_render)-3,16711680);
+				drawRect(left, y1 - ((1000f / Minecraft.getInstance().gameSettings.framerateLimit) * 2), cap * 2, 1, 0, 0, 1, 1);
+				drawRect(left, y1 - (min * 2), cap * 2, 1, 0, 1, 0, 1);
+				drawRect(left, y1 - (max_render), cap * 2, 1, 1, 0, 0, 1);
+				Minecraft.getInstance().fontRenderer.drawString(Minecraft.getInstance().gameSettings.framerateLimit + " FPS", max1, y1 - ((1000f / Minecraft.getInstance().gameSettings.framerateLimit) * 2) - 3, 255);
+				Minecraft.getInstance().fontRenderer.drawString("Minimum " + min + " ms", max1, y1 - (min * 2) - 3, 65280);
+				Minecraft.getInstance().fontRenderer.drawString("Maximum " + max + " ms", max1, y1 - (max_render) - 3, 16711680);
 				Profiler.reset();
 				event.setCanceled(event.isCancelable());
 				Profiler.endSection();
@@ -135,7 +135,7 @@ public class Better_fps_graph {
 		
 		if (event.getType() == RenderGameOverlayEvent.ElementType.DEBUG) {
 			if (event instanceof RenderGameOverlayEvent.Pre) {
-				Profiler.addSection("minecraft:Debug",0,0.5,0);
+				Profiler.addSection("minecraft:Debug", 0, 0.5, 0);
 			} else if (event instanceof RenderGameOverlayEvent.Post) {
 				Profiler.endSection();
 			}
@@ -145,20 +145,20 @@ public class Better_fps_graph {
 	private void onDrawWorldLast(RenderWorldLastEvent event) {
 		framesWithout++;
 		
-		if (framesWithout>=4) {
+		if (framesWithout >= 4) {
 			frames.clear();
 			Profiler.fulReset();
 		}
 	}
 	
-	public static void drawRect(double x,double y,double width,double height,double red,double green,double blue,double alpha) {
-		Tessellator tessellator=Tessellator.getInstance();
-		BufferBuilder buffer=tessellator.getBuffer();
+	public static void drawRect(double x, double y, double width, double height, double red, double green, double blue, double alpha) {
+		Tessellator tessellator = Tessellator.getInstance();
+		BufferBuilder buffer = tessellator.getBuffer();
 		buffer.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_COLOR);
-		buffer.pos(x,(y+height),0).color((float)red,(float)green,(float)blue,(float)alpha).endVertex();
-		buffer.pos((x+width),(y+height),0).color((float)red,(float)green,(float)blue,(float)alpha).endVertex();
-		buffer.pos((x+width),y,0).color((float)red,(float)green,(float)blue,(float)alpha).endVertex();
-		buffer.pos(x,y,0).color((float)red,(float)green,(float)blue,(float)alpha).endVertex();
+		buffer.pos(x, (y + height), 0).color((float) red, (float) green, (float) blue, (float) alpha).endVertex();
+		buffer.pos((x + width), (y + height), 0).color((float) red, (float) green, (float) blue, (float) alpha).endVertex();
+		buffer.pos((x + width), y, 0).color((float) red, (float) green, (float) blue, (float) alpha).endVertex();
+		buffer.pos(x, y, 0).color((float) red, (float) green, (float) blue, (float) alpha).endVertex();
 		tessellator.draw();
 	}
 }
